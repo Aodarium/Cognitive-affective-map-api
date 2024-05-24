341a12a58729446d1ce38973d21ca3c8c7647b75
Aoda <fba.gourelgnail.comt>
2024-05-25 00:18:19 +0200

    Update for vercel

--- api/index.ts
+++ api/index.ts
@@ -4,3 +4,5 @@ import app from './v1/src/app';
 const httpServer = http.createServer(app);
 const PORT: number = process.env.PORT as unknown as number ?? 3001;
 httpServer.listen(PORT, () => console.log(`The server is running on port ${PORT}`))
+
+export default app
