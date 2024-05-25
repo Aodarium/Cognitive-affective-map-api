c04bcc7d9f15d199f2e6a4eb54ed4831b6f60db8
Aoda <fba.gourelgnail.comt>
2024-05-25 08:48:53 +0200

    Update file and config

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,8 @@
 import http from 'http';
 import app from './v1/src/app';
 
-const httpServer = http.createServer(app);
-const PORT: number = process.env.PORT as unknown as number ?? 3001;
-httpServer.listen(PORT, () => console.log(`The server is running on port ${PORT}`))
+// const httpServer = http.createServer(app);
+// const PORT: number = process.env.PORT as unknown as number ?? 3001;
+// httpServer.listen(PORT, () => console.log(`The server is running on port ${PORT}`))
 
 export default app
